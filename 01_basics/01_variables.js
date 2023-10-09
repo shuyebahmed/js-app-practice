cc6beadfc4baba30276c651bb7aa321f6ee7cda6
shuyebahmed <123967858+shuyebahmed@users.noreply.github.com>
2023-10-09 11:05:06 +0000

    Chapter Variables done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,35 @@
+/* 
+# Let, Const, & Var # 
+Information that we are gonna to store on our database for a user based apps like amazon:
+>Name 
+>Email 
+>Password 
+>Username 
+
+const > non changable 
+variable > Changable (var - variable)
+let > Changable (let - variable)
+*/
+
+const accountID = 45112070
+let accountEmail = "shuyebinfo@gmail.com"
+var accountPassword = "234567"
+accountCity = "London"  // Don't use this for variable istead use let always 
+let accountState; // its gonna be undefined 
+
+console.log ("Hello Tester!")
+
+// accountID = 2345
+
+console.log (accountID);
+
+/* 
+Prefer not to use var 
+because of issue in block scope and functional scope
+*/
+accountEmail = "ahmedinfo@gmail.com"
+accountPassword = "93489384"
+accountCity = "Barmingham"
+
+
+console.table([accountEmail, accountPassword, accountCity, accountState])
