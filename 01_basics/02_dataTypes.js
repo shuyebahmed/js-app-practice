3a7ba210df9eb908f4173d0c5c632b204cf7fdf5
shuyebahmed <123967858+shuyebahmed@users.noreply.github.com>
2023-10-09 11:22:47 +0000

    Data Types of Javascript

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -1,7 +1,39 @@
 /* 
+# Datatypes and ECMA standards #
 
+For Doccumentation - 1.mdn  2.tc39 (ecma)
 */ 
 
 "use strict"; // treat all JS code as newer version 
 
 //alert (3+3) //we are using node.js, not browser 
+
+
+console.log("Shuyeb Ahmed")
+
+let name = "Ahmed"
+let age = 22 
+let isLoggedIn = false 
+
+
+// Assignment Of This Chapter - Learn little bit about data types spacification from ecma 
+
+
+// number => 2 to poswer 53 
+// brigint
+// string => " "
+// boolean => true/false 
+// null => standalone value (it's not undefined, it empty)
+// undefined =>  not assigne value untill now 
+// symbol => unique 
+
+
+// object
+
+
+console.log(typeof "Shuyeb"); 
+console.log(typeof "age"); 
+
+console.log(typeof undefined); //undefined 
+console.log(typeof null); // object 
+
